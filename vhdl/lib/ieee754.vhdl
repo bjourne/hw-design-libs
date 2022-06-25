13262960b8ca41b1433849684e6e31931b9bc8d3
Björn Lindqvist <bjourne@gmail.com>
2022-06-25 23:26:52 +0200

    using concatenation op

--- vhdl/lib/ieee754.vhdl
+++ vhdl/lib/ieee754.vhdl
@@ -12,26 +12,25 @@ end package ieee754;
 package body ieee754 is
     pure function float32_to_int32(f : std_logic_vector(31 downto 0))
         return signed is
-        variable s_bexp : signed(7 downto 0);
-        variable s_frac : signed(31 downto 0) := X"00000000";
+        variable bexp : signed(7 downto 0);
+        variable frac : signed(31 downto 0);
     begin
-        s_bexp := signed(f(30 downto 23));
+        bexp := signed(f(30 downto 23));
         -- Zero check.
-        if s_bexp = 0 then
+        if bexp = 0 then
             return to_signed(0, 24);
         end if;
-        s_bexp := s_bexp - 127;
-        s_frac(22 downto 0) := signed(f(22 downto 0));
-        s_frac(23) := '1';
-        if s_bexp < 23 then
-            s_frac := shift_right(s_frac, to_integer(23 - s_bexp));
-        elsif s_bexp > 23 then
-            s_frac := shift_left(s_frac, to_integer(s_bexp - 23));
+        bexp := bexp - 127;
+        frac := "000000001" & signed(f(22 downto 0));
+        if bexp < 23 then
+            frac := shift_right(frac, to_integer(23 - bexp));
+        elsif bexp > 23 then
+            frac := shift_left(frac, to_integer(bexp - 23));
         end if;
         if f(31) = '1' then
-            return -s_frac;
+            return -frac;
         else
-            return s_frac;
+            return frac;
         end if;
     end function;
 end ieee754;
