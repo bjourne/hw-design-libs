174653fd5073e022e0fb21a1af4aed848f9b7da1
Björn Lindqvist <bjourne@gmail.com>
2022-06-25 02:08:16 +0200

    verilog matrix multiplier works

--- verilog/lib/matmul.sv
+++ verilog/lib/matmul.sv
@@ -1,12 +1,113 @@
 // Copyright (C) 2022 Björn A. Lindqvist <bjourne@gmail.com>
+typedef enum {
+    IDLE, READ, CALCULATE, WRITE, ERROR
+} matmul_state_t;
+typedef struct packed {
+    int rows;
+    int cols;
+} matmul_dims_t;
+function int max(int a, int b);
+    return a > b ? a: b;
+endfunction
+function int matmul_compute_time(
+    matmul_dims_t a,
+    matmul_dims_t b);
+    return a.rows * ((b.rows + 1) * b.cols + 1) + 1;
+endfunction
+function int matmul_read_time(
+    matmul_dims_t a,
+    matmul_dims_t b);
+    return max(a.rows * a.cols, b.rows * b.cols);
+endfunction
 
-module divider #(parameter WIDTH=8) (
+
+
+module matmul #(parameter BUF_SIZE=1024) (
     input wire logic clk,
-    input wire logic start
+    input wire logic start,
+    output     matmul_state_t state,
+    input wire matmul_dims_t dims_a,
+    input wire int in_a,
+    input wire matmul_dims_t dims_b,
+    input wire int in_b,
+    output int out_c
 );
+    reg int cnt_a, cnt_b, cnt_c;
+    reg int idx_a, idx_b, idx_c;
+    reg int data_a [0:BUF_SIZE - 1];
+    reg int data_b [0:BUF_SIZE - 1];
+    reg int data_c [0:BUF_SIZE - 1];
+
+    // Iteration variables used during calculation.
+    reg int idx_i, idx_j, idx_k, mac;
+
     always_ff @(posedge clk) begin
+        // Local variables
+        reg int a_ik, b_kj;
         if (start) begin
-        end else if (busy) begin
+            if (dims_a.rows * dims_a.cols < BUF_SIZE &&
+                dims_b.rows * dims_a.cols < BUF_SIZE &&
+                dims_a.cols == dims_b.rows) begin
+                state <= READ;
+                cnt_a <= dims_a.cols * dims_a.rows;
+                cnt_b <= dims_b.cols * dims_b.rows;
+                cnt_c <= dims_a.rows * dims_b.cols;
+                idx_a <= 0;
+                idx_b <= 0;
+                idx_c <= 0;
+            end else begin
+                state <= ERROR;
+            end
+        end else begin
+            if (state == READ) begin
+                if (idx_a < cnt_a) begin
+                    data_a[idx_a] <= in_a;
+                    idx_a <= idx_a + 1;
+                end
+                if (idx_b < cnt_b) begin
+                    data_b[idx_b] <= in_b;
+                    idx_b <= idx_b + 1;
+                end
+                if (idx_a == cnt_a - 1 && idx_b == cnt_b - 1) begin
+                    state <= CALCULATE;
+                    idx_i <= 0;
+                    idx_j <= 0;
+                    idx_k <= 0;
+                    mac <= 0;
+                end
+            end else if (state == ERROR) begin
+                state <= IDLE;
+            end else if (state == CALCULATE) begin
+                if (idx_i == dims_a.rows) begin
+                    state <= WRITE;
+                    idx_c <= 0;
+                end else begin
+                    if (idx_j == dims_b.cols) begin
+                        idx_j <= 0;
+                        idx_i <= idx_i + 1;
+                    end else begin
+                        // This part could be optimized.
+                        if (idx_k == dims_b.rows) begin
+                            data_c[idx_i * dims_b.cols + idx_j] <= mac;
+                            idx_j <= idx_j + 1;
+                            idx_k <= 0;
+                            mac <= 0;
+                        end else begin
+                            a_ik = data_a[idx_i * dims_a.cols + idx_k];
+                            b_kj = data_b[idx_k * dims_b.cols + idx_j];
+                            mac <= mac + a_ik * b_kj;
+                            idx_k <= idx_k + 1;
+                        end
+                    end
+                end
+            end else if (state == WRITE) begin
+                if (idx_c < cnt_c) begin
+                    out_c <= data_c[idx_c];
+                    idx_c <= idx_c + 1;
+                end else begin
+                    state <= IDLE;
+                end
+            end
         end
     end
 endmodule
--- verilog/tb/matmul.sv
+++ verilog/tb/matmul.sv
@@ -1,16 +1,147 @@
 // Copyright (C) 2022 Björn A. Lindqvist <bjourne@gmail.com>
 `include "matmul.sv"
+
 module matmul_tb();
-    localparam WIDTH = 8;
-    localparam BUF_SIZE = 100;
-    reg signed [WIDTH - 1:0] mat_a [0:3];
-    reg signed [WIDTH - 1:0] mat_b [0:3];
+    localparam BUF_SIZE = 1000;
+
+    // Matrix A
+    reg int data_a [0:BUF_SIZE - 1];
+    reg matmul_dims_t dims_a;
+    reg int in_a, mat_a_els;
+
+    // Matrix B
+    reg int data_b [0:BUF_SIZE - 1];
+    reg matmul_dims_t dims_b;
+    reg int in_b, mat_b_els;
+
+    // Matrix C
+    reg int data_c [0:BUF_SIZE - 1];
+    reg int out_c, mat_c_els;
+
+    // General control
+    reg clk, start;
+
+    wire matmul_state_t state;
+
+    task tick; begin
+        #5 clk = ~clk;
+        #5 clk = ~clk;
+    end
+    endtask
+
+    task automatic read_matrices(
+        input string name_a,
+        input string name_b
+    ); begin
+        int r, f;
+        f = $fopen(name_a, "rb");
+        r = $fscanf(f, "%d", dims_a.rows);
+        r = $fscanf(f, "%d", dims_a.cols);
+        for (int i = 0; i < dims_a.rows * dims_a.cols; i++) begin
+            r = $fscanf(f, "%d", data_a[i]);
+        end
+        $fclose(f);
+
+
+        f = $fopen(name_b, "rb");
+        r = $fscanf(f, "%d", dims_b.rows);
+        r = $fscanf(f, "%d", dims_b.cols);
+        for (int i = 0; i < dims_b.rows * dims_b.cols; i++) begin
+            r = $fscanf(f, "%d", data_b[i]);
+            assert(r == 1);
+        end
+        $fclose(f);
+
+        mat_a_els = dims_a.rows * dims_a.cols;
+        mat_b_els = dims_b.rows * dims_b.cols;
+        mat_c_els = dims_a.rows * dims_b.cols;
+    end
+    endtask
 
     initial begin
-        $readmemh("verilog/tb/mat_01_02x02.mem", mat_a);
-        $readmemh("verilog/tb/mat_01_02x02.mem", mat_b);
-        for (integer i = 0; i < 4; i = i + 1)
-            $display("mat_a[%d] %d", i, mat_a[i]);
+        $display(
+            "time start state rows_a cols_a rows_b cols_b in_a in_b out_c"
+        );
+        $monitor(
+            "%4d %5b %5d %6d %6d %6d %6d %4d %4d %5d",
+            $time, start, state,
+            dims_a.rows, dims_a.cols,
+            dims_b.rows, dims_b.cols,
+            in_a, in_b, out_c
+        );
+
+        clk = 1;
+        start = 0;
+        tick;
+
+        // Too large.
+        start = 1;
+        dims_a = {int'(1000), int'(1000)};
+        dims_b = {int'(1000), int'(1000)};
+        tick;
+        assert(state == ERROR);
+
+        start = 0;
+        tick;
+        assert(state == IDLE);
+
+        // Dimension mismatch.
+        dims_a = {int'(2), int'(3)};
+        dims_b = {int'(4), int'(2)};
+        start = 1;
+        tick;
+        assert(state == ERROR);
+
+        start = 0;
+        tick;
+        assert(state == IDLE);
+
+        // Now for real
+        read_matrices(
+            "verilog/tb/mat_01_02x02.mem",
+            "verilog/tb/mat_01_02x02.mem");
+        start = 1;
+        tick;
+
+        assert(state == READ);
+        start = 0;
+
+        for (int i = 0;
+             i < matmul_read_time(dims_a, dims_b);
+             i = i + 1) begin
+            if (i < mat_a_els) begin
+                in_a = data_a[i];
+            end
+            if (i < mat_b_els) begin
+                in_b = data_b[i];
+            end
+            tick;
+        end
+        assert(state == CALCULATE);
+
+        repeat (matmul_compute_time(dims_a, dims_b))
+            tick;
+
+        assert(state == WRITE);
+        for (int i = 0; i < mat_c_els; i = i + 1) begin
+            tick;
+            data_c[i] = out_c;
+        end
+        tick;
+
+        assert(state == IDLE);
+        assert(data_c[0] == 700);
+        assert(data_c[1] == 1000);
+        assert(data_c[2] == 1500);
+        assert(data_c[3] == 2200);
+
+        #5 $finish;
     end
 
+    matmul matmul (
+        clk, start, state,
+        dims_a, in_a,
+        dims_b, in_b,
+        out_c
+    );
 endmodule
