15927fd44e0839d0459f055db9883d9c206c3727
Björn Lindqvist <bjourne@gmail.com>
2022-07-26 22:44:30 +0200

    WIP: more inferencing code

---
+++ hwgraph/hwgraph/inferencing2.py
@@ -0,0 +1,120 @@
+# Copyright (C) 2022 Björn A. Lindqvist <bjourne@gmail.com>
+from ast import *
+from collections import Counter, defaultdict
+from fractions import Fraction
+from functools import reduce
+
+def parse_expr(expr):
+    return parse(expr).body[0].value
+
+def multivariate(terms):
+    return defaultdict(int, terms)
+
+def multivariate_const(c):
+    return multivariate({() : c})
+
+def multivariate_name(id):
+    return multivariate({((id, 1),) : 1})
+
+def elwise(mv1, mv2, fun):
+    combos = list(mv1) + list(mv2)
+    return multivariate({c : fun(mv1[c], mv2[c]) for c in combos})
+
+def add(mv1, mv2):
+    return elwise(mv1, mv2, lambda x, y: x + y)
+
+def sub(mv1, mv2):
+    return elwise(mv1, mv2, lambda x, y: x - y)
+
+def pow(mv1, mv2):
+    items = list(mv2.items())
+    pows, n = items[0]
+    err = 'Positive integer powers only'
+    assert len(items) == 1 and pows == () and n > 0, err
+    mv3 = multivariate_const(1)
+    return reduce(lambda x, _: mul(x, mv1), range(n), mv3)
+
+def mul(mv1, mv2):
+    mv3 = multivariate_const(0)
+    for es1, c1 in mv1.items():
+        for es2, c2 in mv2.items():
+            es3 = Counter(dict(es1))
+            es3.update(dict(es2))
+            es3 = tuple(sorted(es3.items()))
+            mv3[es3] += c1*c2
+    return mv3
+
+BINOPS = {
+    Add : add, Sub : sub,
+    Mult : mul, Pow : pow
+}
+
+def eval(tree, vars):
+    tp = type(tree)
+    if tp == BinOp:
+        l = eval(tree.left, vars)
+        r = eval(tree.right,vars )
+        return BINOPS[type(tree.op)](l, r)
+    elif tp == Name:
+        id = tree.id
+        if id in vars:
+            return multivariate_const(vars[id])
+        return multivariate_name(id)
+    elif tp == Constant:
+        return multivariate({() : tree.value})
+    elif tp == UnaryOp:
+        a = eval(tree.operand, vars)
+        return mul(a, multivariate_const(-1))
+
+def constrain2(mv, tp_op):
+    by_var = defaultdict(lambda: defaultdict(int))
+    for var_pows, c in mv.items():
+        if c != 0:
+            for v, p in var_pows:
+                by_var[v][p] = c
+    c_term = mv[()]
+
+    n_vars = len(by_var)
+    if n_vars == 0:
+        if tp_op == Gt:
+            if c_term > 0:
+                return 'satisfies'
+            else:
+                return 'nosol'
+        elif tp_op == Lt:
+            if c_term < 0:
+                return 'satisfies'
+            else:
+                return 'nosol'
+        elif tp_op == Eq:
+            if c_term == 0:
+                return 'satisfies'
+            else:
+                return 'nosol'
+    elif n_vars > 1:
+        # Can't constrain multiple variables.
+        return 'many'
+
+    by_var = list(by_var.items())
+    var, cs = by_var[0]
+    cs[0] = c_term
+
+    # Don't get this.
+    p, q = -cs[0], cs[1]
+    sp, sq = p < 0, q < 0
+
+    cmps = {Gt : '>', Eq : '==', Lt : '<'}
+    if (sp, sq) == (True, True):
+        cmps = {Gt : '<', Eq : '==', Lt : '>'}
+    elif (sp, sq) == (False, True):
+        cmps = {Gt : '<', Eq : '==', Lt : '>'}
+    return var, cmps[tp_op], Fraction(p, q)
+
+
+def constrain(vars, expr):
+    tree = parse_expr(expr)
+    lhs, op, rhs = tree.left, tree.ops[0], tree.comparators[0]
+    tree = BinOp(lhs, Sub(), rhs)
+    mv = eval(tree, vars)
+
+    return constrain2(mv, type(op))
--- hwgraph/tests/test_inferencing.py
+++ hwgraph/tests/test_inferencing.py
@@ -1,34 +1,86 @@
 # Copyright (C) 2022 Björn A. Lindqvist <bjourne@gmail.com>
 from hwgraph import Type, Vertex, connect_vertices
-from hwgraph.inferencing import forward, input_arities
+from hwgraph.inferencing2 import constrain
 
-def test_input_arities():
-    c1 = Vertex('c1', Type('const', [], ['o']))
-    c1.output['o'].arity = 5
-    add = Vertex('sum', Type('add', ['i1', 'i2'], ['o']))
+def test_no_solutions():
+    assert constrain({}, '10 < 0') == 'nosol'
+    assert constrain({}, '0 < 0') == 'nosol'
+    assert constrain({}, '3 < 3') == 'nosol'
+    assert constrain({}, '3 > 3') == 'nosol'
+    assert constrain({}, '-10 > 0') == 'nosol'
+    assert constrain({}, '-10 > -5') == 'nosol'
+    assert constrain({}, '10 == 5') == 'nosol'
 
-    connect_vertices(c1, 'o', add)
-    connect_vertices(c1, 'o', add)
-    assert input_arities(add) == [5, 5]
+    assert constrain({'x' : 3}, 'x == 5') == 'nosol'
+    assert constrain({'x' : 3}, 'x > 5') == 'nosol'
 
-def test_forward_add():
-    c1 = Vertex('c1', Type('const', [], ['o']))
-    c1.output['o'].arity = 5
-    add = Vertex('v', Type('add', ['i1', 'i2'], ['o']))
+def test_many_solutions():
+    assert constrain({}, 'x > y') == 'many'
 
-    connect_vertices(c1, 'o', add)
-    connect_vertices(c1, 'o', add)
-    assert forward(add)
+def test_satisfies():
+    assert constrain({}, '10 > 0') == 'satisfies'
+    assert constrain({}, '-10 > -11') == 'satisfies'
+    vars = {'hi' : 3, 'lo' : 0}
+    assert constrain(vars, 'hi > lo') == 'satisfies'
 
-    assert add.output['o'].arity == 5
+def test_basic():
+    vars = {'hi' : 20, 'lo' : 9}
+    res = constrain(vars, 'hi - lo == o')
+    assert res == ('o', '==', 11)
 
-def test_forward_cat():
-    c1 = Vertex('c1', Type('const', [], ['o']))
-    c1.output['o'].arity = 5
-    cat = Vertex('v', Type('cat', ['i1', 'i2'], ['o']))
+    vars = {'hi' : 20, 'lo' : 9, 'o' : 50}
+    res = constrain(vars, 'hi - lo == o')
+    assert res == 'nosol'
 
-    connect_vertices(c1, 'o', cat)
-    connect_vertices(c1, 'o', cat)
-    assert forward(cat)
+    res = constrain({}, 'hi - lo == o')
+    assert res == 'many'
 
-    assert cat.output['o'].arity == 10
+    vars = {'n' : 3}
+    assert constrain(vars, 'n == i') == ('i', '==', 3)
+
+def test_ineqs():
+    vars = {'lo' : 5}
+    assert constrain(vars, 'hi > lo') == ('hi', '>', 5)
+    assert constrain({}, '8 > o') == ('o', '<', 8)
+    assert constrain({}, '8 > -o') == ('o', '>', -8)
+    assert constrain({}, '-8 > o') == ('o', '<', -8)
+    assert constrain({}, '-8 > -o') == ('o', '>', 8)
+
+    assert constrain({}, '8 < o') == ('o', '>', 8)
+    assert constrain({}, '8 < -o') == ('o', '<', -8)
+    assert constrain({}, '-8 < o') == ('o', '>', -8)
+    assert constrain({}, '-8 < -o') == ('o', '<', 8)
+
+
+
+
+# def test_input_arities():
+#     c1 = Vertex('c1', Type('const', [], ['o']))
+#     c1.output['o'].arity = 5
+#     add = Vertex('sum', Type('add', ['i1', 'i2'], ['o']))
+
+#     connect_vertices(c1, 'o', add)
+#     connect_vertices(c1, 'o', add)
+#     assert input_arities(add) == [5, 5]
+
+# def test_forward_add():
+#     c1 = Vertex('c1', Type('const', [], ['o']))
+#     c1.output['o'].arity = 5
+#     add = Vertex('v', Type('add', ['i1', 'i2'], ['o']))
+
+#     connect_vertices(c1, 'o', add)
+#     connect_vertices(c1, 'o', add)
+#     assert forward(add)
+
+#     assert add.output['o'].arity == 5
+
+# def test_forward_cat():
+#     c1 = Vertex('c1', Type('const', [], ['o']))
+#     c1.output['o'].arity = 5
+#     cat = Vertex('v', Type('cat', ['i1', 'i2'], ['o']))
+
+#     connect_vertices(c1, 'o', cat)
+#     connect_vertices(c1, 'o', cat)
+#     assert forward(cat)
+
+#     assert cat.output['o'].arity == 10
