1467184660118eb4f9db0adbbc781344d8c2feec
Björn Lindqvist <bjourne@gmail.com>
2022-07-31 16:55:47 +0200

    simpler interface for the parity

--- vhdl/lib/parity.vhdl
+++ vhdl/lib/parity.vhdl
@@ -5,19 +5,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- par is the bit that should be added to d to make the sum of the 1s
+-- odd or even.
 entity parity is
+
     port(
         d : in std_logic_vector(7 downto 0);
-        even : in std_logic;
-        odd : in std_logic;
-        sum_even : out std_logic;
-        sum_odd : out std_logic
+        odd_even : in std_logic;
+        par : out std_logic
     );
 end entity;
 
 architecture rtl of parity is
-    signal not_parry, parry : std_logic;
-
 begin
     -- parry <= (((d(0) xnor d(1)) xor (d(2) xnor d(3))) xnor
     --           ((d(4) xnor d(5)) xor (d(6) xnor d(7))));
@@ -25,7 +24,8 @@ begin
 
     -- sum_even <= (parry nand odd) and (not_parry nand even);
     -- sum_odd <= (parry nand even) and (not_parry nand odd);
+    par <= xor (d & odd_even);
 
-    sum_even <= (xor d) xor even;
-    sum_odd <= not sum_even;
+    -- sum_even <= (xor (d & even));
+    -- sum_odd <= not sum_even;
 end architecture;
--- vhdl/tb/tb_parity.vhdl
+++ vhdl/tb/tb_parity.vhdl
@@ -9,53 +9,38 @@ entity tb_parity is
 end tb_parity;
 
 architecture beh of tb_parity is
-    signal bits : std_logic_vector(7 downto 0);
-    signal even : std_logic;
-    signal odd : std_logic;
-    signal sum_even : std_logic;
-    signal sum_odd : std_logic;
+    signal d : std_logic_vector(7 downto 0);
+    signal odd_even : std_logic;
+    signal par : std_logic;
+    -- signal odd : std_logic;
+    -- signal sum_even : std_logic;
+    -- signal sum_odd : std_logic;
 begin
     parity0: entity parity
         port map (
-            d => bits,
-            even => even,
-            odd => odd,
-            sum_even => sum_even,
-            sum_odd => sum_odd
+            d => d,
+            odd_even => odd_even,
+            par => par
         );
 
     process
     begin
-        bits <= "00000000";
-        even <= '1';
-        odd <= '0';
+        d <= "00000000";
+        odd_even <= '1';
         wait for 10 ns;
-        assert sum_even = '1';
-        assert sum_odd = '0';
+        assert par = '1';
 
-        bits <= "00000001";
+        odd_even <= '0';
         wait for 10 ns;
-        assert sum_even = '0';
-        assert sum_odd = '1';
+        assert par = '0';
 
-        bits <= "00000111";
+        d <= "00000001";
         wait for 10 ns;
-        assert sum_even = '0';
-        assert sum_odd = '1';
+        assert par = '1';
 
-        -- Sum even with even=0
-        even <= '0';
-        odd <= '1';
-        bits <= "00000000";
+        d <= "00000111";
         wait for 10 ns;
-        assert sum_even = '0';
-        assert sum_odd = '1';
-
-        bits <= "00000001";
-        wait for 10 ns;
-        assert sum_even = '1';
-        assert sum_odd = '0';
-
+        assert par = '1';
 
         assert false report "all tests passed" severity note;
         wait;
