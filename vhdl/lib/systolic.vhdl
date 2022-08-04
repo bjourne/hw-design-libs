d446979d6f07999314d1c4f3ec20478ce4001546
Björn Lindqvist <bjourne@gmail.com>
2022-08-04 09:29:29 +0200

    making code more generic

--- vhdl/lib/systolic.vhdl
+++ vhdl/lib/systolic.vhdl
@@ -1,24 +1,27 @@
 -- Copyright (C) 2022 Björn A. Lindqvist <bjourne@gmail.com>
 library ieee;
-use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
 
 entity systolic is
+    generic(
+        N : integer
+    );
     port(
         clk : in std_logic;
         rstn : in std_logic;
         in_valid : in std_logic;
         in_ready : out std_logic;
-        a_row : in integer_vector(0 to 2);
-        b_col : in integer_vector(0 to 2);
-        c_out : out integer_vector(0 to 2)
+        a_row : in integer_vector(0 to N - 1);
+        b_col : in integer_vector(0 to N - 1);
+        c_out : out integer_vector(0 to N - 1)
     );
 end entity;
 
 architecture rtl of systolic is
-    type east_t is array(0 to 4, 0 to 5) of integer;
-    type south_t is array(0 to 5, 0 to 4) of integer;
-    type south_east_t is array(0 to 5, 0 to 5) of integer;
+    type east_t is array(0 to N + 1, 0 to N + 2) of integer;
+    type south_t is array(0 to N + 2, 0 to N + 1) of integer;
+    type south_east_t is array(0 to N + 2, 0 to N + 2) of integer;
 
     signal cnt : natural;
     signal p : std_logic;
@@ -32,7 +35,7 @@ architecture rtl of systolic is
         report "*** Tick #" & to_string(cnt) & " "
             & to_string(in_valid) & " ***";
         report "E:";
-        for r in 0 to 4 loop
+        for r in 0 to N + 1 loop
             report to_string(r) & ": "
                 & to_string(E(r, 0)) & " "
                 & to_string(E(r, 1)) & " "
@@ -42,7 +45,7 @@ architecture rtl of systolic is
                 & to_string(E(r, 5));
         end loop;
         report "SE:";
-        for r in 0 to 5 loop
+        for r in 0 to N + 2 loop
             report to_string(r) & ": "
                 & to_string(SE(r, 0)) & " "
                 & to_string(SE(r, 1)) & " "
@@ -75,15 +78,10 @@ begin
         end if;
 
         -- Systolic array handling
-        SE(0, 0) <= 0;
-        SE(0, 1) <= 0;
-        SE(0, 2) <= 0;
-        SE(0, 3) <= 0;
-        SE(0, 4) <= 0;
-        SE(1, 0) <= 0;
-        SE(2, 0) <= 0;
-        SE(3, 0) <= 0;
-        SE(4, 0) <= 0;
+        for i in 0 to N + 1 loop
+            SE(0, i) <= 0;
+            SE(i, 0) <= 0;
+        end loop;
         case next_cnt is
             when 0 =>
                 E(0, 0) <= a_row(0);
@@ -122,27 +120,29 @@ begin
                 S(0, 3) <= b_col(1);
                 S(0, 4) <= b_col(2);
             when 4 =>
-                c_out <= (SE(3, 4), SE(4, 4), SE(4, 3));
+                c_out(0) <= SE(3, 4);
+                c_out(1) <= SE(4, 4);
+                c_out(2) <= SE(4, 3);
             when 5 =>
-                c_out <= (SE(3, 5), SE(4, 4), SE(5, 3));
+                c_out(0) <= SE(3, 5);
+                c_out(1) <= SE(4, 4);
+                c_out(2) <= SE(5, 3);
             when 6 =>
-                c_out <= (SE(4, 5), SE(5, 5), SE(5, 4));
+                c_out(0) <= SE(4, 5);
+                c_out(1) <= SE(5, 5);
+                c_out(2) <= SE(5, 4);
             when 7 =>
-                c_out <= (SE(5, 5), SE(5, 5), SE(5, 5));
+                c_out(0) <= SE(5, 5);
+                c_out(1) <= SE(5, 5);
+                c_out(2) <= SE(5, 5);
             when others =>
-                E(0, 0) <= 0;
-                E(1, 0) <= 0;
-                E(2, 0) <= 0;
-                E(3, 0) <= 0;
-                E(4, 0) <= 0;
-
-                S(0, 0) <= 0;
-                S(0, 1) <= 0;
-                S(0, 2) <= 0;
-                S(0, 3) <= 0;
-                S(0, 4) <= 0;
-
-                c_out <= (0, 0, 0);
+                for i in 0 to N + 1 loop
+                    E(i, 0) <= 0;
+                    S(0, i) <= 0;
+                end loop;
+                for i in 0 to N - 1 loop
+                    c_out(i) <= 0;
+                end loop;
         end case;
         if rising_edge(clk) then
             for r in 0 to 4 loop
--- vhdl/tb/tb_systolic.vhdl
+++ vhdl/tb/tb_systolic.vhdl
@@ -1,11 +1,4 @@
 -- Copyright (C) 2022 Björn A. Lindqvist <bjourne@gmail.com>
--- A = [[15, 2, 3],
---      [4, 5, 6],
---      [7, 8, 9]]
--- B = [[10, 11, 12],
---      [13, 14, 15],
---      [16, 17, 18]]
-
 library bjourne;
 library ieee;
 use ieee.std_logic_1164.all;
@@ -16,6 +9,14 @@ entity tb_systolic is
 end tb_systolic;
 
 architecture beh of tb_systolic is
+    signal clk : std_logic;
+    signal rstn : std_logic;
+    signal in_valid3, in_ready3 : std_logic;
+    signal a_row3, b_col3, c_out3 : integer_vector(0 to 2);
+
+    signal in_valid4, in_ready4 : std_logic;
+    signal a_row4, b_col4, c_out4 : integer_vector(0 to 3);
+
     procedure tick(signal c : inout std_logic) is
     begin
         wait for 10 ns;
@@ -23,66 +24,90 @@ architecture beh of tb_systolic is
         wait for 10 ns;
         c <= not c;
     end;
-    signal clk : std_logic;
-    signal rstn : std_logic;
-    signal in_valid, in_ready : std_logic;
-    signal a_row, b_col, c_out : integer_vector(0 to 2);
+
+    procedure reset(signal clk0 : inout std_logic;
+                    signal rstn0 : inout std_logic) is
+    begin
+        clk0 <= '0';
+        rstn0 <= '0';
+        wait for 10 ns;
+        tick(clk0);
+
+        rstn0 <= '1';
+        tick(clk0);
+    end;
 begin
-    systolic0: entity systolic
+    systolic3: entity systolic
+        generic map(
+            N => 3
+        )
         port map (
             clk => clk,
             rstn => rstn,
-            in_valid => in_valid,
-            in_ready => in_ready,
-            a_row => a_row,
-            b_col => b_col,
-            c_out => c_out
+            in_valid => in_valid3,
+            in_ready => in_ready3,
+            a_row => a_row3,
+            b_col => b_col3,
+            c_out => c_out3
+        );
+    systolic4: entity systolic
+        generic map(
+            N => 4
+        )
+        port map (
+            clk => clk,
+            rstn => rstn,
+            in_valid => in_valid4,
+            in_ready => in_ready4,
+            a_row => a_row4,
+            b_col => b_col4,
+            c_out => c_out4
         );
     process
     begin
-        clk <= '0';
-        rstn <= '0';
-        wait for 10 ns;
+        reset(clk, rstn);
+
+        assert in_ready4 = '1';
+        in_valid4 <= '1';
+
+        a_row4 <= (3, 6, 9, 0);
+        b_col4 <= (9, 2, 4, 4);
         tick(clk);
 
-        rstn <= '1';
-        tick(clk);
-
-
-        assert in_ready = '1';
-        in_valid <= '1';
+        assert in_ready3 = '1';
+        in_valid3 <= '1';
         -- Tick 0
-        a_row <= (15, 2, 3);
-        b_col <= (10, 13, 16);
+        a_row3 <= (15, 2, 3);
+        b_col3 <= (10, 13, 16);
         tick(clk);
 
         -- Tick 1
-        in_valid <= '0';
-        a_row <= (4, 5, 6);
-        b_col <= (11, 14, 17);
+        in_valid3 <= '0';
+        a_row3 <= (4, 5, 6);
+        b_col3 <= (11, 14, 17);
         tick(clk);
 
         -- Tick 2
-        a_row <= (7, 8, 9);
-        b_col <= (12, 15, 18);
+        a_row3 <= (7, 8, 9);
+        b_col3 <= (12, 15, 18);
         tick(clk);
 
         -- Tick 3
-        a_row <= (0, 0, 0);
-        b_col <= (0, 0, 0);
+        a_row3 <= (0, 0, 0);
+        b_col3 <= (0, 0, 0);
 
         tick(clk);
-        assert c_out = (244, 224, 201);
+        assert c_out3 = (244, 224, 201);
 
         tick(clk);
-        assert c_out = (264, 216, 318);
+        assert c_out3 = (264, 216, 318);
 
         tick(clk);
-        assert c_out = (231, 216, 342);
+        assert c_out3 = (231, 216, 342);
 
         tick(clk);
-        assert c_out = (366, 366, 366);
-        assert in_ready = '1';
+        assert c_out3 = (366, 366, 366);
+        assert in_ready3 = '1';
 
 
         assert false report "all tests passed" severity note;
