43849f0c240ae0032ad97dd11c19b4b5e38fa9cb
Björn Lindqvist <bjourne@gmail.com>
2023-01-17 20:51:20 +0100

    types

--- vhdl/lib/types.vhdl
+++ vhdl/lib/types.vhdl
@@ -1,7 +1,9 @@
 library bjourne;
 library ieee;
+use ieee.float_pkg.all;
 use ieee.numeric_std.all;
 
 package types is
     type real_array2d_t is array(natural range<>) of real_vector;
+    type arr1d_float32 is array(natural range<>) of float32;
 end package;
