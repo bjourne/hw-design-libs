5e6afee39c7e6c820093cf6f429c1e114e934ff3
Björn Lindqvist <bjourne@gmail.com>
2023-05-10 10:15:55 +0200

    attempt to model random latency

--- vhdl/lib/pl/adder.vhdl
+++ vhdl/lib/pl/adder.vhdl
@@ -1,14 +1,13 @@
 -- Copyright (C) 2023 Björn A. Lindqvist <bjourne@gmail.com>
 -- Need to figure out how subpackages work.
+library bjourne;
 library bjourne_pl;
 library ieee;
+use bjourne.utils.all;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 
 entity adder is
-    generic (
-        LATENCY : positive := 4
-    );
     port (
         clk, nrst : in std_logic;
 
@@ -33,6 +32,9 @@ architecture rtl of adder is
     signal i : integer;
 begin
     process(clk)
+        variable lat : integer;
+        variable seed1 : positive := 999;
+        variable seed2 : positive := 999;
     begin
         if rising_edge(clk) then
             if nrst = '0' then
@@ -41,29 +43,32 @@ begin
                 d0_v <= '0';
                 i <= 0;
             else
-                if u0_v and u0_r then
-                    u0_r <= '0';
-                    o0 <= u0_d;
-                end if;
-                if u1_v and u1_r then
-                    u1_r <= '0';
-                    o1 <= u1_d;
-                end if;
-
-                if (u0_r = '0') and (u1_r = '0') and (i = 0) then
-                    i <= LATENCY;
-                    d0_v <= '0';
-                elsif i > 0 then
-                    i <= i - 1;
+                -- Idle state
+                if i = 0 then
+                    -- Transfer inputs
+                    if u0_v and u0_r then
+                        u0_r <= '0';
+                        o0 <= u0_d;
+                    end if;
+                    if u1_v and u1_r then
+                        u1_r <= '0';
+                        o1 <= u1_d;
+                    end if;
+                    -- Begin computation
+                    if (u0_r = '0') and (u1_r = '0')  then
+                        rand_range(seed1, seed2, 7, lat);
+                        i <= 2 + lat;
+                        d0_v <= '0';
+                    end if;
                 else
-                    i <= 0;
-                end if;
-                if i = 1 then
-                    d0_d <= o0 + o1;
-                    d0_v <= '1';
-
-                    u0_r <= '1';
-                    u1_r <= '1';
+                    -- Transer outputs
+                    if i = 1 then
+                        d0_d <= o0 + o1;
+                        d0_v <= '1';
+                        u0_r <= '1';
+                        u1_r <= '1';
+                    end if;
+                    i <= i - 1;
                 end if;
             end if;
         end if;
